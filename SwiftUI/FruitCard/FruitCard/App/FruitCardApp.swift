f228e8336b6e5a8e9cb208202e03e7bf724b63a5
calmkeen <iyunseong159@gmail.com>
2023-04-30 17:29:09 +0900

    push Test

---
+++ SwiftUI/FruitCard/FruitCard/App/FruitCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitCardApp.swift
+//  FruitCard
+//
+//  Created by calmkeen on 2023/04/29.
+//
+
+import SwiftUI
+
+@main
+struct FruitCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
---
+++ SwiftUI/FruitCard/FruitCard/App/OnboardingView.swift
@@ -0,0 +1,27 @@
+//
+//  OnboardingView.swift
+//  FruitCard
+//
+//  Created by calmkeen on 2023/04/30.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        TabView {
+            ForEach(0..<5) { item in
+                FruitCardView()
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+        
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
