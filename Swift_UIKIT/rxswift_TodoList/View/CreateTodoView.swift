e9fd396f403a9f6c3aac0a1b9b6ac4570bcc24fa
Younsung <78361650+calmkeen@users.noreply.github.com>
2023-10-01 15:20:08 +0900

    feat: RxInit
    Test Rx todoList init

---
+++ Swift_UIKIT/rxswift_TodoList/View/CreateTodoView.swift
@@ -0,0 +1,54 @@
+//
+//  CreateView.swift
+//  rxswift_TodoList
+//
+//  Created by calmkeen on 2023/06/24.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+class CreateTodoView : UIViewController {
+    
+
+    var titleField = UITextField.then {
+        
+    }
+    var detailField = UITextField.then {
+        
+    }
+    var dataLabel = UILabel.then {
+        
+    }
+    var datePciker = Date.then {
+        
+    }
+    var importantValuelabel = UILabel.then {
+        
+    }
+    //pull down Button
+    var important = UIAction.then {
+        
+    }
+    
+    var tagLabel  = UILabel.then {
+        
+    }
+    var tagButton = UIAction.then {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func makeView() {
+        
+    }
+    
+    func makeConstraints() {
+        
+    }
+}
---
+++ Swift_UIKIT/rxswift_TodoList/View/InfoView.swift
@@ -0,0 +1,10 @@
+//
+//  InfoView.swift
+//  rxswift_TodoList
+//
+//  Created by calmkeen on 2023/06/24.
+//
+
+import UIKit
+import Then
+import SnapKit
